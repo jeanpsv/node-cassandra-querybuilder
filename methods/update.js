55804c6a8dbd2f21fe5c0205405d869fe039310b
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2016-04-18 11:31:46 -0300

    close #11 - implements update method

---
+++ methods/update.js
@@ -0,0 +1,76 @@
+var reqlib = require('app-root-path').require;
+
+var Builder = reqlib('/builder/builder');
+var Operator = reqlib('/operators/operator');
+var Where = reqlib('/conditions/where');
+
+
+/**
+ * inheritance
+ */
+Update.prototype = Object.create(Builder.prototype);
+
+
+/**
+ * Insert constructor
+ */
+function Update() {
+	Builder.call(this);
+	this._operators = [];
+	this._where = new Where();
+};
+
+
+/**
+ * set database and table
+ * @param  {string} database database name
+ * @param  {string} table    table name
+ * @return {Update}          the instance
+ */
+Update.prototype.from = function(database, table) {
+	Builder.prototype.from.call(this, database, table);
+	return this;
+};
+
+/**
+ * set assignment
+ * @param {Operator} assignment assignment to recorded
+ * @return {Update}             the instance
+ */
+Update.prototype.set = function(assignment) {
+	this._operators.push(assignment.toString());
+	return this;
+};
+
+/**
+ * add condition
+ * @param  {Operator} condition clause condition
+ * @return {Update}             the instance
+ */
+Update.prototype.where = function(clause) {
+	this._where.where(clause);
+	return this;
+};
+
+/**
+ * concat condition
+ * @param  {Operator} clause condition
+ * @return {Update}          the instance
+ */
+Update.prototype.and = function(clause) {
+	this._where.and(clause);
+	return this;
+};
+
+Update.prototype.toString = function() {
+	var to_string = ['UPDATE'];
+	to_string.push(Builder.prototype.toString.call(this, false));
+	to_string.push('SET');
+	to_string.push(this._operators.join(', '));
+	to_string.push(this._where.toString() + ';');
+	return to_string.join(' ');
+};
+
+
+/*----------  Module exports  ----------*/
+module.exports = Update;
---
+++ test/methods/update.js
@@ -0,0 +1,82 @@
+process.env.NODE_ENV = 'test';
+
+var reqlib = require('app-root-path').require;
+var assert = require('assert');
+
+var UPDATE = reqlib('/methods/update');
+var OPERATOR = reqlib('/operators/operator');
+var FROM = reqlib('/util/from');
+
+describe('Update', function() {
+	describe('#constructor', function() {
+		it('should create an instance of Update', function() {
+			var u = new UPDATE();
+			assert.ok(u);
+		});
+	});
+	describe('#from', function() {
+		it('should set database and table', function() {
+			var u = new UPDATE();
+			assert.ok(u.from('database', 'table'));
+		});
+	});
+	describe('#where', function() {
+		it('should set a condition', function() {
+			var u = new UPDATE();
+			var o = new OPERATOR();
+			var column = 'col';
+			var value = 'val';
+			o.eq(column, value);
+			assert.ok(u.where(o));
+		});
+	});
+	describe('#and', function() {
+		it('should append a condition', function() {
+			var u = new UPDATE();
+			var o1 = new OPERATOR();
+			var column1 = 'col1';
+			var value1 = 'val1';
+			o1.eq(column1, value1);
+			var o2 = new OPERATOR();
+			var column2 = 'col2';
+			var value2 = 'val2';
+			o2.eq(column2, value2);
+			assert.ok(u.where(o1).and(o2));
+		});
+	});
+	describe('#toString', function() {
+		it('should get string representation of the instance', function() {
+			var database = 'database';
+			var table = 'table';
+			var f = new FROM();
+			f.from(database, table);
+			var u = new UPDATE();
+			var o1 = new OPERATOR();
+			var column1 = 'col1';
+			var value1 = 'val1';
+			o1.eq(column1, value1);
+			var o2 = new OPERATOR();
+			var column2 = 'col2';
+			var value2 = 'val2';
+			o2.eq(column2, value2);
+			var o3 = new OPERATOR();
+			var column3 = 'col3';
+			var value3 = 'val3';
+			o3.eq(column3, value3);
+			var o4 = new OPERATOR();
+			var column4 = 'col4';
+			var value4 = 'val4';
+			o4.lt(column4, value4);
+			var to_string = [];
+			to_string.push('UPDATE');
+			to_string.push(f.toString(false));
+			to_string.push('SET');
+			to_string.push([o1.toString(), o2.toString()].join(', '));
+			to_string.push('WHERE');
+			to_string.push(o3.toString());
+			to_string.push('AND');
+			to_string.push(o4.toString() + ';');
+			assert.equal(u.from(database, table).set(o1).set(o2).where(o3).and(o4).toString(), to_string.join(' '));
+		});
+	});
+});
